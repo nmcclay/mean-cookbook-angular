d013a8a569733da48a24b8e718a9fa668d38b403
Nicholas McClay <nmcclay@wombatsecurity.com>
2017-04-15 14:42:36 -0400

    Added a nested module, built out some routing examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {RouterModule, PreloadAllModules} from '@angular/router';
 import { AppComponent } from './app.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthorComponent } from './author/author.component';
+import {PostsModule} from "./posts/posts.module";
+
+const ROUTES = [{
+  path: 'authors',
+  component: AuthorsComponent,
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorsComponent,
+    PageNotFoundComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES),
+    PostsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
