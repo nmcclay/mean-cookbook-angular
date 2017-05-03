283b2bb0608975dd69db051c4fb59b03e73a7ee6
Nicholas McClay <nmcclay@wombatsecurity.com>
2017-05-03 19:59:40 -0400

    wired post service to Blogger API

--- src/app/posts/blog-posts.service.ts
+++ src/app/posts/blog-posts.service.ts
@@ -1,21 +1,31 @@
 import { Injectable } from '@angular/core';
 import {BlogPost} from "./blog-post";
-
-export const POSTS: BlogPost[] = [
-  new BlogPost('How data-binding works', "coming soon..."),
-  new BlogPost('What\'s the deal with directives?', "coming soon..."),
-  new BlogPost('Cha-cha-ng-ges...', "coming soon..."),
-  new BlogPost('Components & services', "coming soon..."),
-  new BlogPost('Express.js in a nutshell', "coming soon..."),
-  new BlogPost('Hu-MONGO-ous DB', "coming soon..."),
-  new BlogPost('Component Style Secrets', "coming soon..."),
-  new BlogPost('Angular Deployment', "coming soon..."),
-  new BlogPost('Internationalization Tips', "coming soon..."),
-];
+import {Http} from "@angular/http";
+import { environment } from '../../environments/environment';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class BlogPostsService {
+  private apiHostUrl = 'https://www.googleapis.com';
+  private blogUrl = this.apiHostUrl + '/blogger/v3/blogs/7159470537406093899';
+  private postsUrl = this.blogUrl + '/posts?key=' +
+    environment.bloggerAPIKey;
+
+  constructor(private http: Http) {}
+
   getPosts(): Promise<BlogPost[]> {
-    return Promise.resolve(POSTS);
+    return this.http.get(this.postsUrl)
+      .toPromise()
+      .then((response) => {
+        let posts = response.json().items;
+        console.log(posts);
+        return posts as BlogPost[]
+      })
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
