cf41caf0c92ef47496c1cd31b9a0ac10a9aa5244
Nicholas McClay <nmcclay@wombatsecurity.com>
2017-09-16 13:34:12 -0400

    Upgraded libraries to latest versions

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from "@angular/router";
-import {PostsComponent} from "./posts.component";
-import {PostComponent} from "./post/post.component";
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from "@angular/router";
+import { PostsComponent } from "./posts.component";
+import { PostComponent } from "./post/post.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
 
 
